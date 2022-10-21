8f8d77a23ef71e85830a3635c41d27198f5eeb30
Brennan Shacklett <bpshacklett@gmail.com>
2022-10-20 23:10:14 -0700

    Add profiling to single world version of simple example

--- examples/simple_jobs/main.cpp
+++ examples/simple_jobs/main.cpp
@@ -10,29 +10,54 @@
 #include "simple.hpp"
 #include "init.hpp"
 
+#include <chrono>
+
 using namespace madrona;
 
 namespace SimpleExample {
 
-static void launch()
+static void launch(uint32_t num_benchmark_ticks)
 {
     StateManager state_mgr;
 
-    EnvInit env_init = generateEnvironmentInitialization(0);
+    EnvInit env_init = generateEnvironmentInitialization(num_benchmark_ticks);
     
+    auto start = std::chrono::steady_clock::now();
     JobManager job_mgr(JobManager::makeEntry<Engine>(
         [&env_init](Engine &ctx) {
             SimpleSim::entry(ctx, env_init);
         }), 0, 0, &state_mgr);
     
     job_mgr.waitForAllFinished();
+    auto end = std::chrono::steady_clock::now();
+
+    double duration = std::chrono::duration<double>(end - start).count();
+
+    if (num_benchmark_ticks > 0) {
+        printf("FPS: %f, Elapsed: %f\n",
+               (double)num_benchmark_ticks / duration, duration);
+    }
 
     free(env_init.objsInit);
 }
 
 }
 
-int main(int, char **)
+int main(int argc, char *argv[])
 {
-    SimpleExample::launch();
+    uint64_t num_benchmark_ticks = 0;
+    if (argc >= 2 && !strcmp(argv[1], "--bench")) {
+        if (argc == 2) {
+            fprintf(stderr, "Usage: %s [--bench NUM_TICKS]\n", argv[0]);
+            return EXIT_FAILURE;
+        }
+
+        num_benchmark_ticks = std::stoul(argv[2]);
+        if (num_benchmark_ticks == 0) {
+            fprintf(stderr, "%s: NUM_TICKS must be > 0 in benchmark mode", argv[0]);
+            return EXIT_FAILURE;
+        }
+    }
+
+    SimpleExample::launch(num_benchmark_ticks);
 }
