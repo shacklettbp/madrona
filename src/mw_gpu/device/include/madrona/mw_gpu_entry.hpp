14fab67383f7b9b650975a4db4985b1e3e0e93cc
Brennan Shacklett <bpshacklett@gmail.com>
2022-12-03 19:35:04 -0800

    Add missing entry headers

---
+++ src/mw_gpu/device/include/madrona/mw_gpu_entry.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <madrona/taskgraph.hpp>
+
+namespace madrona {
+namespace mwGPU {
+namespace entryKernels {
+
+template <typename ContextT, typename WorldDataT, typename InitT>
+__global__ void initECS()
+{
+    StateManager *state_mgr = mwGPU::getStateManager();
+    new (state_mgr) StateManager(0);
+
+    ECSRegistry ecs_registry(*state_mgr);
+    WorldDataT::registerTypes(ecs_registry);
+}
+
+template <typename ContextT, typename WorldDataT, typename InitT>
+__global__ void initWorlds(int32_t num_worlds,
+                           void *inits_raw)
+{
+    InitT *inits = (InitT *)inits_raw;
+
+    for (int32_t world_idx = 0; world_idx < num_worlds; world_idx++) {
+        const InitT &init = inits[world_idx];
+        WorldBase *world = TaskGraph::getWorld(world_idx);
+
+        ContextT ctx =
+            mwGPU::EntryBase<ContextT, WorldDataT>::makeContext(WorldID {
+                world_idx,
+            });
+
+        new (world) WorldDataT(ctx, init);
+    }
+}
+
+template <typename ContextT, typename WorldDataT, typename InitT>
+__global__ void initTasks()
+{
+    TaskGraph::Builder builder(1024, 1024);
+    WorldDataT::setupTasks(builder);
+
+    builder.build((TaskGraph *)mwGPU::GPUImplConsts::get().taskGraph);
+}
+
+}
+
+template <typename ContextT, typename WorldDataT, typename InitT,
+          decltype(entryKernels::initECS<ContextT, WorldDataT, InitT>) =
+              entryKernels::initECS<ContextT, WorldDataT, InitT>,
+          decltype(entryKernels::initWorlds<ContextT, WorldDataT, InitT>) =
+              entryKernels::initWorlds<ContextT, WorldDataT, InitT>,
+          decltype(entryKernels::initTasks<ContextT, WorldDataT, InitT>) =
+              entryKernels::initTasks<ContextT, WorldDataT, InitT>
+         >
+struct alignas(16) MWGPUEntry {};
+
+}
+}
+
+// This macro forces MWGPUEntry to be instantiated, which in turn instantiates
+// the entryKernels::* __global__ entry points. static_assert with a trivially
+// true check leaves no side effects in the scope where this macro is called.
+#define MADRONA_BUILD_MWGPU_ENTRY(ContextT, WorldT, InitT) \
+    static_assert(\
+        alignof(::madrona::mwGPU::MWGPUEntry<ContextT, WorldT, InitT>) == 16);
