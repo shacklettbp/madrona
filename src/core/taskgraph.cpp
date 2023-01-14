c00d01de58652b1014cb06c3d6741575b4b3dedf
Brennan Shacklett <bpshacklett@gmail.com>
2023-01-14 13:22:25 -0800

    CPU - renderer integration

--- src/core/taskgraph.cpp
+++ src/core/taskgraph.cpp
@@ -7,20 +7,6 @@
 #include "worker_init.hpp"
 
 namespace madrona {
-namespace {
-
-CountT getNumCores()
-{
-    int os_num_threads = sysconf(_SC_NPROCESSORS_ONLN);
-
-    if (os_num_threads == -1) {
-        FATAL("Failed to get number of concurrent threads");
-    }
-
-    return os_num_threads;
-}
-
-}
 
 TaskGraph::Builder::Builder(Context &ctx)
     : ctx_(&ctx),
@@ -132,108 +118,6 @@ void TaskGraph::run(Context *ctx)
 {
     for (const Node &node : sorted_nodes_) {
         node.fn((NodeBase *)(&node_datas_[node.dataIDX].userData[0]), ctx);
-    }
-}
-
-ThreadPoolExecutor::ThreadPoolExecutor(CountT num_worlds, CountT num_workers)
-    : workers_(num_workers == 0 ? getNumCores() : num_workers),
-      worker_wakeup_(0),
-      main_wakeup_(0),
-      current_jobs_(nullptr),
-      num_jobs_(0),
-      next_job_(0),
-      num_finished_(0),
-      state_mgr_(num_worlds),
-      state_caches_(num_worlds)
-{
-    for (CountT i = 0; i < num_worlds; i++) {
-        new (&state_caches_[i]) StateCache();
-    }
-
-    for (CountT i = 0; i < workers_.size(); i++) {
-        new (&workers_[i]) std::thread([this]() {
-            workerThread();
-        });
-    }
-}
-
-ThreadPoolExecutor::~ThreadPoolExecutor()
-{
-    worker_wakeup_.store(-1, std::memory_order_release);
-
-    for (CountT i = 0; i < workers_.size(); i++) {
-        workers_[i].join();
-    }
-}
-
-void ThreadPoolExecutor::run(Job *jobs, CountT num_jobs)
-{
-    current_jobs_ = jobs;
-    num_jobs_ = uint32_t(num_jobs);
-    next_job_.store(0, std::memory_order_relaxed);
-    num_finished_.store(0, std::memory_order_relaxed);
-    worker_wakeup_.store(1, std::memory_order_release);
-    worker_wakeup_.notify_all();
-
-    main_wakeup_.wait(0, std::memory_order_acquire);
-    main_wakeup_.store(0, std::memory_order_relaxed);
-}
-
-void ThreadPoolExecutor::ctxInit(void (*init_fn)(void *, const WorkerInit &),
-                                 void *init_data, CountT world_idx)
-{
-    WorkerInit worker_init {
-        &state_mgr_,
-        &state_caches_[world_idx],
-        uint32_t(world_idx),
-    };
-
-    init_fn(init_data, worker_init);
-}
-
-
-ECSRegistry ThreadPoolExecutor::getECSRegistry()
-{
-    return ECSRegistry(&state_mgr_, nullptr); // FIXME
-}
-
-void ThreadPoolExecutor::workerThread()
-{
-    while (true) {
-        worker_wakeup_.wait(0, std::memory_order_relaxed);
-
-        int32_t ctrl = worker_wakeup_.load(std::memory_order_acquire);
-        if (ctrl == 0) {
-            continue;
-        } else if (ctrl == -1) {
-            break;
-        }
-
-        while (true) {
-            // FIXME: Is there a potential overflow here if a thread doesn't
-            // see that worker_wakeup_ becomes 0 again?
-            uint32_t job_idx =
-                next_job_.fetch_add(1, std::memory_order_relaxed);
-            if (job_idx == num_jobs_) {
-                worker_wakeup_.store(0, std::memory_order_relaxed);
-            }
-
-            if (job_idx >= num_jobs_) {
-                break;
-            }
-
-            current_jobs_[job_idx].fn(current_jobs_[job_idx].data);
-
-            // This has to be acq_rel so the finishing thread has seen
-            // all the other threads' effects
-            uint32_t prev_finished =
-                num_finished_.fetch_add(1, std::memory_order_acq_rel);
-
-            if (prev_finished == num_jobs_ - 1) {
-                main_wakeup_.store(1, std::memory_order_release);
-                main_wakeup_.notify_one();
-            }
-        }
     }
 }
 
