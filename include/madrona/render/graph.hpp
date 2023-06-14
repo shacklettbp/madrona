dddc3f3a9bd9d5e24c5b6a6dce5b1999d7ab1f60
Brennan Shacklett <bpshacklett@gmail.com>
2023-06-14 11:37:29 -0700

    wip

--- include/madrona/render/graph.hpp
+++ include/madrona/render/graph.hpp
@@ -39,6 +39,30 @@ struct CopyTaskArgs {
     bool forceDMA = false;
 };
 
+struct TaskParamBlock {
+};
+
+struct TaskArgs {
+    Span<const TaskResource> read = {};
+    Span<const TaskResource> write = {};
+    Span<const TaskResource> readwrite = {};
+};
+
+// Question: how we do we pass in the actual DescriptorSetLayout to the rendergraph
+// Core issue: Ideally it would be great if the rendergraph could setup ParamBlock
+// for each pass and bind physical resources to the paramblock. Then before the user code is called in the render loop it could bind the paramblock. and bind the pipeline before user's function is called. The problem is this makes a major assumption: 1. all the commands in the task need all the inputs and outputs specified. For RasterTasks, this is fairly straightforward for framebuffer attachments as a reasonable assumption seems to be that a given raster task MUST all be in a single render pass. However, what if the output of a prior pass is literally the index buffer for the next pass (or a acel structure build pass)? We don't even want to bind the pass inputs as a paramblock at that point
+//
+// - Also we don't want to do a 1-1 mapping between pipelines and graph tasks.
+//
+// A1: Does the rendergraph get the Shader object as input?
+//  - No it doesn't. The rendergraph needs to be rebuilt on window resize due to
+//    resource resizing. Compiling the shaders to pipelines needs to happen before
+// A2: ParamBlock management is out of scope for the rendergraph. Pass a "setup" function to addXXX task when just receives
+// resources as inputs and then does binding manually.
+//   - Makes it hard / impossible to help manage descriptor set allocation across the pass. The issue is that if the setup functions are called sequentially, they need to create the concrete ParamBlocks at time of call. One option would be to pass the param block data requirements in the TaskArgs (I will need XYZ descriptor sets with N things => setup runs and these are passed in??). But wait, at this point why not just have a span of paramblocks in the task definition?
+//   - Seems like it would allow a single addTask function. Could imagine helper functions layered on top like "addRenderPassTask" which does the renderpass setup for you.
+//   - The most natural implementation of this seems like it would be the setup callback actually returning the run callback, which captures stuff in setup by value. Seems REALLLY ugly in terms of code you write though (nested lambdas).
+
 class RenderGraphBuilder {
 private:
     struct LogicalResource;
@@ -52,15 +76,17 @@ public:
     inline TaskResource addBuffer(BufferDesc desc);
 
     template <typename Fn>
-    inline void addRasterTask(Fn &&fn, RasterTaskArgs args);
+    inline void addRasterTask(RasterTaskArgs args,
+                              Fn &&fn);
 
     template <typename Fn>
-    inline void addComputeTask(Fn &&fn, ComputeTaskArgs args);
+    inline void addComputeTask(ComputeTaskArgs args,
+                               Fn &&fn);
 
     template <typename Fn>
-    inline void addCopyTask(Fn &&fn, CopyTaskArgs args);
+    inline void addCopyTask(CopyTaskArgs args, Fn &&fn);
 
-    RenderGraph build(GPU &gpu, CountT num_inflight);
+    RenderGraph build(GPU &gpu, CountT num_frames);
 
 private:
     inline LogicalResource * addResource();
