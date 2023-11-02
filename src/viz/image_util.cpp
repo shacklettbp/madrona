880b5ce36b95356efbea064ca5fcfe2c2cc25ecd
llGuy <luc.rosenzweig@yahoo.com>
2023-11-01 19:39:57 -0700

    Removed warnings from image_util.cpp

--- src/viz/image_util.cpp
+++ src/viz/image_util.cpp
@@ -1,6 +1,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-function"
+#pragma clang diagnostic ignored "-Wmissing-field-initializers"
 #include <stb_image.h>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
