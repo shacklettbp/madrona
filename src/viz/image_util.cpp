06b8adfb1a2d21a2d8b2e418157dbd81de914ff7
llGuy <luc.rosenzweig@yahoo.com>
2023-11-01 19:57:05 -0700

    Cleaned warning fix

--- src/viz/image_util.cpp
+++ src/viz/image_util.cpp
@@ -1,9 +1,5 @@
 #define STB_IMAGE_IMPLEMENTATION
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-function"
-#pragma clang diagnostic ignored "-Wmissing-field-initializers"
 #include <stb_image.h>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
-#pragma clang diagnostic pop
