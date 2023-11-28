fe605ff9914b762acc27ed39ccd243e7afad1f4f
ZanderMajercik <majercik@stanford.edu>
2023-11-27 17:25:16 -0800

    ScanNode draft implementation

--- src/mw/device/sort_archetype.cpp
+++ src/mw/device/sort_archetype.cpp
@@ -49,6 +49,472 @@ inline constexpr int MAX_NUM_PASSES =
  ******************************************************************************/
 using namespace cub;
 
+/******************************************************************************
+ * Thread block abstractions
+ ******************************************************************************/
+
+/**
+ * @brief AgentScan implements a stateful abstraction of CUDA thread blocks for
+ *        participating in device-wide prefix scan.
+ * @tparam AgentScanPolicyT
+ *   Parameterized AgentScanPolicyT tuning policy type
+ *
+ * @tparam InputIteratorT
+ *   Random-access input iterator type
+ *
+ * @tparam OutputIteratorT
+ *   Random-access output iterator type
+ *
+ * @tparam ScanOpT
+ *   Scan functor type
+ *
+ * @tparam InitValueT
+ *   The init_value element for ScanOpT type (cub::NullType for inclusive scan)
+ *
+ * @tparam OffsetT
+ *   Signed integer type for global offsets
+ *
+ */
+struct SortArchetypeNodeBase::ScanCustom
+{
+    using InitValueT = int32_t;
+    using OffsetT = int32_t;
+    using AccumT = int32_t;
+  //---------------------------------------------------------------------
+  // Types and constants
+  //---------------------------------------------------------------------
+
+  // The input value type
+  using InputT = int32_t;
+
+  // Tile status descriptor interface type
+  using ScanTileStateT = ScanTileState<AccumT>;
+
+  // Constants
+  enum
+  {
+    // Inclusive scan if no init_value type is provided
+    IS_INCLUSIVE     = false,
+    BLOCK_THREADS    = consts::numMegakernelThreads,
+    ITEMS_PER_THREAD = 1, // TODO: restore, might need num_elems_per_sort_thread_ i.e. 2
+    TILE_ITEMS       = BLOCK_THREADS * ITEMS_PER_THREAD,
+  };
+
+  // Parameterized BlockLoad type
+  typedef BlockLoad<AccumT,
+                    BLOCK_THREADS,
+                    ITEMS_PER_THREAD>
+    BlockLoadT;
+
+  // Parameterized BlockStore type
+  typedef BlockStore<AccumT,
+                     BLOCK_THREADS,
+                     ITEMS_PER_THREAD>
+    BlockStoreT;
+
+  // Parameterized BlockScan type
+  typedef BlockScan<AccumT,
+                    BLOCK_THREADS>
+    BlockScanT;
+
+  // Callback type for obtaining tile prefix during block scan
+  typedef TilePrefixCallbackOp<AccumT, ScanOpT, ScanTileStateT>
+    TilePrefixCallbackOpT;
+
+  // Stateful BlockScan prefix callback type for managing a running total while
+  // scanning consecutive tiles
+  typedef BlockScanRunningPrefixOp<AccumT, ScanOpT> RunningPrefixCallbackOp;
+
+  // Shared memory type for this thread block
+  union _TempStorage
+  {
+    // Smem needed for tile loading
+    typename BlockLoadT::TempStorage load;
+
+    // Smem needed for tile storing
+    typename BlockStoreT::TempStorage store;
+
+    struct ScanStorage
+    {
+      // Smem needed for cooperative prefix callback
+      typename TilePrefixCallbackOpT::TempStorage prefix;
+
+      // Smem needed for tile scanning
+      typename BlockScanT::TempStorage scan;
+    } scan_storage;
+  };
+
+  // Alias wrapper allowing storage to be unioned
+  struct TempStorage : Uninitialized<_TempStorage>
+  {};
+
+  //---------------------------------------------------------------------
+  // Per-thread fields
+  //---------------------------------------------------------------------
+
+  _TempStorage &temp_storage; ///< Reference to temp_storage
+  const OffsetT *d_in; ///< Input data
+  OffsetT* d_out;      ///< Output data
+  ScanOpT scan_op;            ///< Binary scan operator
+  InitValueT init_value;      ///< The init_value element for ScanOpT
+
+  //---------------------------------------------------------------------
+  // Block scan utility methods
+  //---------------------------------------------------------------------
+
+  /**
+   * Exclusive scan specialization (first tile)
+   */
+  __device__ __forceinline__ void ScanTile(AccumT (&items)[ITEMS_PER_THREAD],
+                                           AccumT init_value,
+                                           ScanOpT scan_op,
+                                           AccumT &block_aggregate,
+                                           Int2Type<false> /*is_inclusive*/)
+  {
+    BlockScanT(temp_storage.scan_storage.scan)
+      .ExclusiveScan(items, items, init_value, scan_op, block_aggregate);
+    block_aggregate = scan_op(init_value, block_aggregate);
+  }
+
+  /**
+   * Inclusive scan specialization (first tile)
+   */
+  __device__ __forceinline__ void ScanTile(AccumT (&items)[ITEMS_PER_THREAD],
+                                           InitValueT /*init_value*/,
+                                           ScanOpT scan_op,
+                                           AccumT &block_aggregate,
+                                           Int2Type<true> /*is_inclusive*/)
+  {
+    BlockScanT(temp_storage.scan_storage.scan)
+      .InclusiveScan(items, items, scan_op, block_aggregate);
+  }
+
+  /**
+   * Exclusive scan specialization (subsequent tiles)
+   */
+  template <typename PrefixCallback>
+  __device__ __forceinline__ void ScanTile(AccumT (&items)[ITEMS_PER_THREAD],
+                                           ScanOpT scan_op,
+                                           PrefixCallback &prefix_op,
+                                           Int2Type<false> /*is_inclusive*/)
+  {
+    BlockScanT(temp_storage.scan_storage.scan)
+      .ExclusiveScan(items, items, scan_op, prefix_op);
+  }
+
+  /**
+   * Inclusive scan specialization (subsequent tiles)
+   */
+  template <typename PrefixCallback>
+  __device__ __forceinline__ void ScanTile(AccumT (&items)[ITEMS_PER_THREAD],
+                                           ScanOpT scan_op,
+                                           PrefixCallback &prefix_op,
+                                           Int2Type<true> /*is_inclusive*/)
+  {
+    BlockScanT(temp_storage.scan_storage.scan)
+      .InclusiveScan(items, items, scan_op, prefix_op);
+  }
+
+  //---------------------------------------------------------------------
+  // Constructor
+  //---------------------------------------------------------------------
+
+  /**
+   * @param temp_storage
+   *   Reference to temp_storage
+   *
+   * @param d_in
+   *   Input data
+   *
+   * @param d_out
+   *   Output data
+   *
+   * @param scan_op
+   *   Binary scan operator
+   *
+   * @param init_value
+   *   Initial value to seed the exclusive scan
+   */
+  __device__ __forceinline__ ScanCustom(TempStorage &temp_storage,
+                                       InputIteratorT d_in,
+                                       OutputIteratorT d_out,
+                                       InitValueT init_value)
+      : temp_storage(temp_storage.Alias())
+      , d_in(d_in)
+      , d_out(d_out)
+      , scan_op(cub::Sum())
+      , init_value(init_value)
+  {}
+
+  //---------------------------------------------------------------------
+  // Cooperatively scan a device-wide sequence of tiles with other CTAs
+  //---------------------------------------------------------------------
+
+  /**
+   * Process a tile of input (dynamic chained scan)
+   * @tparam IS_LAST_TILE
+   *   Whether the current tile is the last tile
+   *
+   * @param num_remaining
+   *   Number of global input items remaining (including this tile)
+   *
+   * @param tile_idx
+   *   Tile index
+   *
+   * @param tile_offset
+   *   Tile offset
+   *
+   * @param tile_state
+   *   Global tile state descriptor
+   */
+  template <bool IS_LAST_TILE>
+  __device__ __forceinline__ void ConsumeTile(OffsetT num_remaining,
+                                              int tile_idx,
+                                              OffsetT tile_offset,
+                                              ScanTileStateT &tile_state)
+  {
+    // Load items
+    AccumT items[ITEMS_PER_THREAD];
+
+    if (IS_LAST_TILE)
+    {
+      // Fill last element with the first element because collectives are
+      // not suffix guarded.
+      BlockLoadT(temp_storage.load)
+        .Load(d_in + tile_offset, items, num_remaining, *(d_in + tile_offset));
+    }
+    else
+    {
+      BlockLoadT(temp_storage.load).Load(d_in + tile_offset, items);
+    }
+
+    CTA_SYNC();
+
+    // Perform tile scan
+    if (tile_idx == 0)
+    {
+      // Scan first tile
+      AccumT block_aggregate;
+      ScanTile(items,
+               init_value,
+               scan_op,
+               block_aggregate,
+               Int2Type<IS_INCLUSIVE>());
+
+      if ((!IS_LAST_TILE) && (threadIdx.x == 0))
+      {
+        tile_state.SetInclusive(0, block_aggregate);
+      }
+    }
+    else
+    {
+      // Scan non-first tile
+      TilePrefixCallbackOpT prefix_op(tile_state,
+                                      temp_storage.scan_storage.prefix,
+                                      scan_op,
+                                      tile_idx);
+      ScanTile(items, scan_op, prefix_op, Int2Type<IS_INCLUSIVE>());
+    }
+
+    CTA_SYNC();
+
+    // Store items
+    if (IS_LAST_TILE)
+    {
+      BlockStoreT(temp_storage.store)
+        .Store(d_out + tile_offset, items, num_remaining);
+    }
+    else
+    {
+      BlockStoreT(temp_storage.store).Store(d_out + tile_offset, items);
+    }
+  }
+
+  /**
+   * @brief Scan tiles of items as part of a dynamic chained scan
+   *
+   * @param num_items
+   *   Total number of input items
+   *
+   * @param tile_state
+   *   Global tile state descriptor
+   *
+   * @param start_tile
+   *   The starting tile for the current grid
+   */
+  __device__ __forceinline__ void ConsumeRange(OffsetT num_items,
+                                               ScanTileStateT &tile_state,
+                                               int start_tile)
+  {
+    // Blocks are launched in increasing order, so just assign one tile per
+    // block
+
+    // Current tile index
+    int tile_idx = start_tile + blockIdx.x;
+
+    // Global offset for the current tile
+    OffsetT tile_offset = OffsetT(TILE_ITEMS) * tile_idx;
+
+    // Remaining items (including this tile)
+    OffsetT num_remaining = num_items - tile_offset;
+
+    if (num_remaining > TILE_ITEMS)
+    {
+      // Not last tile
+      ConsumeTile<false>(num_remaining, tile_idx, tile_offset, tile_state);
+    }
+    else if (num_remaining > 0)
+    {
+      // Last tile
+      ConsumeTile<true>(num_remaining, tile_idx, tile_offset, tile_state);
+    }
+  }
+
+  //---------------------------------------------------------------------------
+  // Scan an sequence of consecutive tiles (independent of other thread blocks)
+  //---------------------------------------------------------------------------
+
+  /**
+   * @brief Process a tile of input
+   *
+   * @param tile_offset
+   *   Tile offset
+   *
+   * @param prefix_op
+   *   Running prefix operator
+   *
+   * @param valid_items
+   *   Number of valid items in the tile
+   */
+  template <bool IS_FIRST_TILE, bool IS_LAST_TILE>
+  __device__ __forceinline__ void ConsumeTile(OffsetT tile_offset,
+                                              RunningPrefixCallbackOp &prefix_op,
+                                              int valid_items = TILE_ITEMS)
+  {
+    // Load items
+    AccumT items[ITEMS_PER_THREAD];
+
+    if (IS_LAST_TILE)
+    {
+      // Fill last element with the first element because collectives are
+      // not suffix guarded.
+      BlockLoadT(temp_storage.load)
+        .Load(d_in + tile_offset, items, valid_items, *(d_in + tile_offset));
+    }
+    else
+    {
+      BlockLoadT(temp_storage.load).Load(d_in + tile_offset, items);
+    }
+
+    CTA_SYNC();
+
+    // Block scan
+    if (IS_FIRST_TILE)
+    {
+      AccumT block_aggregate;
+      ScanTile(items,
+               init_value,
+               scan_op,
+               block_aggregate,
+               Int2Type<IS_INCLUSIVE>());
+      prefix_op.running_total = block_aggregate;
+    }
+    else
+    {
+      ScanTile(items, scan_op, prefix_op, Int2Type<IS_INCLUSIVE>());
+    }
+
+    CTA_SYNC();
+
+    // Store items
+    if (IS_LAST_TILE)
+    {
+      BlockStoreT(temp_storage.store)
+        .Store(d_out + tile_offset, items, valid_items);
+    }
+    else
+    {
+      BlockStoreT(temp_storage.store).Store(d_out + tile_offset, items);
+    }
+  }
+
+  /**
+   * @brief Scan a consecutive share of input tiles
+   *
+   * @param[in] range_offset
+   *   Threadblock begin offset (inclusive)
+   *
+   * @param[in] range_end
+   *   Threadblock end offset (exclusive)
+   */
+  __device__ __forceinline__ void ConsumeRange(OffsetT range_offset,
+                                               OffsetT range_end)
+  {
+    BlockScanRunningPrefixOp<AccumT, ScanOpT> prefix_op(scan_op);
+
+    if (range_offset + TILE_ITEMS <= range_end)
+    {
+      // Consume first tile of input (full)
+      ConsumeTile<true, true>(range_offset, prefix_op);
+      range_offset += TILE_ITEMS;
+
+      // Consume subsequent full tiles of input
+      while (range_offset + TILE_ITEMS <= range_end)
+      {
+        ConsumeTile<false, true>(range_offset, prefix_op);
+        range_offset += TILE_ITEMS;
+      }
+
+      // Consume a partially-full tile
+      if (range_offset < range_end)
+      {
+        int valid_items = range_end - range_offset;
+        ConsumeTile<false, false>(range_offset, prefix_op, valid_items);
+      }
+    }
+    else
+    {
+      // Consume the first tile of input (partially-full)
+      int valid_items = range_end - range_offset;
+      ConsumeTile<true, false>(range_offset, prefix_op, valid_items);
+    }
+  }
+
+  /**
+   * @brief Scan a consecutive share of input tiles, seeded with the
+   *        specified prefix value
+   * @param[in] range_offset
+   *   Threadblock begin offset (inclusive)
+   *
+   * @param[in] range_end
+   *   Threadblock end offset (exclusive)
+   *
+   * @param[in] prefix
+   *   The prefix to apply to the scan segment
+   */
+  __device__ __forceinline__ void ConsumeRange(OffsetT range_offset,
+                                               OffsetT range_end,
+                                               AccumT prefix)
+  {
+    BlockScanRunningPrefixOp<AccumT, ScanOpT> prefix_op(prefix, scan_op);
+
+    // Consume full tiles of input
+    while (range_offset + TILE_ITEMS <= range_end)
+    {
+      ConsumeTile<true, false>(range_offset, prefix_op);
+      range_offset += TILE_ITEMS;
+    }
+
+    // Consume a partially-full tile
+    if (range_offset < range_end)
+    {
+      int valid_items = range_end - range_offset;
+      ConsumeTile<false, false>(range_offset, prefix_op, valid_items);
+    }
+  }
+};
+
+
 /**
  * Radix-rank using matching which computes the counts of keys for each digit
  * value early, at the expense of doing more work. This may be useful e.g. for
@@ -1174,12 +1640,7 @@ void SortArchetypeNodeBase::worldCountScan(int32_t block_idx)
     int32_t invocation_idx = 
         block_idx * consts::numMegakernelThreads + threadIdx.x;
 
-    uint32_t worldCountVals[1];
-    uint32_t worldOffsetVals[1];
-    worldCountVals[0] = worldCounts[invocation_idx];
-    worldOffsetVals[0] = worldOffsets[invocation_idx];
-
-    BlockScanT(*smem_tmp).ExclusiveSum(worldCountVals, worldOffsetVals);
+    BlockScanT(*smem_tmp).ExclusiveSum(worldCounts[invocation_idx], worldOffsets[invocation_idx]);
 
     if (invocation_idx == 0) {
         int32_t numEntities = bins[(numPasses - 1) * 256 + 255];
