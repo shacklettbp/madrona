8a22bb9e4e2e20cfc86091fd0288481f6ebb614d
Zander <majercik@cs.stanford.edu>
2024-09-26 14:55:29 -0700

    reverted merge of old taskgraph.hpp and sort_archetype.hpp

--- src/mw/device/include/madrona/taskgraph.hpp
+++ src/mw/device/include/madrona/taskgraph.hpp
@@ -309,13 +309,6 @@ struct SortArchetypeNodeBase : NodeBase {
         void onesweep(int32_t invocation_idx);
     };
 
-    struct ScanNode : NodeBase {
-        ScanNode(ParentNodeT parent);
-        ParentNodeT parentNode;
-
-        void scan(int32_t invocation_idx);
-    }
-
     struct RearrangeNode : NodeBase {
         RearrangeNode(uint32_t taskgraph_id, ParentNodeT parent,
                       int32_t col_idx);
