631ee8482776cc59cf6644e61729e10e9c654619
Brennan Shacklett <bpshacklett@gmail.com>
2024-10-01 12:36:30 -0700

    Add int type aliases

--- include/madrona/types.hpp
+++ include/madrona/types.hpp
@@ -5,22 +5,32 @@
 
 namespace madrona {
 
-inline constexpr uint32_t operator "" _u32(unsigned long long v) 
+using u64 = uint64_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using i32 = int32_t;
+using u16 = uint16_t;
+using i16 = int16_t;
+using u8 = uint8_t;
+using i8 = int8_t;
+using f32 = float;
+
+inline constexpr u32 operator "" _u32(unsigned long long v) 
 { 
     return uint32_t(v);
 }
 
-inline constexpr uint64_t operator "" _u64(unsigned long long v) 
+inline constexpr u64 operator "" _u64(unsigned long long v) 
 { 
     return uint64_t(v);
 }
 
-inline constexpr int32_t operator "" _i32(unsigned long long v) 
+inline constexpr i32 operator "" _i32(unsigned long long v) 
 { 
     return int32_t(v);
 }
 
-inline constexpr int64_t operator "" _i64(unsigned long long v) 
+inline constexpr i64 operator "" _i64(unsigned long long v) 
 { 
     return int64_t(v);
 }
