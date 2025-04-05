3db59bf54b22ca4c9ad5657ab7ffa215e31c11d5
llGuy <luc.rosenzweig@yahoo.com>
2025-04-05 16:19:27 -0700

    starting perf analysis

--- src/physics/physics_impl.hpp
+++ src/physics/physics_impl.hpp
@@ -2,6 +2,95 @@
 
 #include <madrona/physics.hpp>
 
+#ifdef MADRONA_GPU_MODE
+// #define CV_COUNT_GPU_CLOCKS
+#endif
+
+
+
+#ifdef CV_COUNT_GPU_CLOCKS
+extern "C" {
+struct CVClocks {
+    AtomicU64 com;
+    AtomicU64 interias;
+
+    // RNE with just G
+    AtomicU64 rneG;
+    // RNE with full acceleration as input
+    AtomicU64 rne;
+
+    AtomicU64 crb;
+    AtomicU64 invMass;
+
+    AtomicU64 processContacts;
+
+    AtomicU64 convert;
+
+    AtomicU64 destroy;
+    AtomicU64 init;
+
+    // Integration
+    AtomicU64 intg;
+
+    AtomicU64 fk;
+
+    AtomicU64 narrowphase;
+
+    AtomicU64 allocScratch;
+    AtomicU64 prepSolver;
+    AtomicU64 contAccRef;
+    AtomicU64 eqAccRef;
+    AtomicU64 cg;
+};
+
+extern CVClocks cvClocks;
+}
+
+class CVClockHelper {
+public:
+    inline CVClockHelper(AtomicU64 &counter)
+        : counter_(&counter)
+    {
+        cuda::atomic_thread_fence(cuda::memory_order_seq_cst,
+                                  cuda::thread_scope_thread);
+        start_ = timestamp();
+    }
+
+    inline void end()
+    {
+        cuda::atomic_thread_fence(cuda::memory_order_seq_cst,
+                                  cuda::thread_scope_thread);
+        auto end = timestamp();
+        counter_->fetch_add_relaxed(end - start_);
+        counter_ = nullptr;
+    }
+
+    inline ~CVClockHelper()
+    {
+        if (counter_ != nullptr) {
+            end();
+        }
+    }
+
+private:
+    inline uint64_t timestamp() const
+    {
+        uint64_t v;
+        asm volatile("mov.u64 %0, %%globaltimer;"
+                     : "=l"(v));
+        return v;
+    }
+
+    AtomicU64 *counter_;
+    uint64_t start_;
+};
+
+#define CV_PROF_START(name, counter) \
+    CVClockHelper name(cvClocks. counter)
+
+#define CV_PROF_END(name) name.end()
+#endif
+
 namespace madrona::phys {
 
 struct PhysicsSystemState {
